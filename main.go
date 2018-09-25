f01e68cbc9dfbb3fac4729a249d53f50d2b6e4f7
Zain Patel <zain.patel06@gmail.com>
2018-09-25 01:27:48 +0100

    adding the main program with support for csingle command line flag

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/mzjp2/drive-share/drive"
+)
+
+func main() {
+	fileName := flag.String("name", "default", " The file name of the file you want to share")
+	flag.Parse()
+	fmt.Printf("%s\n", *fileName)
+
+	drive.ListFiles("name contains 'Job'")
+}
